aa46debdc63c6555d89230f9cda0d1252972695c
Patrick Bentley <pbentley07@gmail.com>
2015-12-02 20:22:40 -0500

    Current Site with Volunteer Page

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public_html/css"
+sass_dir = "sass"
+images_dir = "public_html/images"
+
+# Use non-compressed/debug compiling in development
+# sass_options = { :debug_info => true }
+output_style = :expanded
+
+# Use compressed/debug compiling when in production and comment out development-specific options
+#output_style = :compressed
+
+# syntax
+preferred_syntax = :scss
+
